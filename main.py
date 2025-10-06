a5c42740d0994a49c20f3fd333c1838408c41531
val-0715 <valeriem97@nycstudents.net>
2025-10-06 12:53:54 +0000

    functions greeting user and total before tax

--- main.py
+++ main.py
@@ -1,3 +1,19 @@
 restaurant_name = 'Gyu-Kaku'
 restaurant_address = '44-45 21st St, Long Island City, NY 11101'
 bill_subtotal = 148.58
+floating_number= float(bill_subtotal)
+#to make sure the number could be a float
+
+
+#GREETING USER
+def greeting_user ():
+    print("Welcome to the NYC Tip Calculator!")
+
+#GETTING PRE-TAX TOTAL
+def get_bill_total (bill_subtotal):
+    print (f"Your total before tax is {bill_subtotal}")
+
+#PRINTING OUT THE RECIEPT
+greeting_user()
+print("------------------------------------------------------")
+get_bill_total(bill_subtotal)
